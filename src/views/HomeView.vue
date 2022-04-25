9f694517748cf5d7b3ca7049ebea57d314dbd100
Raj Kumar Shrestha <shresthar27@gmail.com>
2022-04-26 00:04:43 +0545

    added a separate button for current location trigger

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,20 @@
     <div class="z-20 flex justify-center relative px-4 pt-8 pb-32 bg-hero-pattern bg-cover">
 
       <!-- search input -->
-      <div class="w-full max-w-screen-small">
+      <div class="w-full max-w-screen-sm">
         <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
 
         <div class="flex">
-          <input v-model="queryIP" type="text" class="flex-1 py-3 px-3 rounded-tl-md rounded-bl-md focus:outline-none" placeholder="Search for any IP address or leave empty to get your IP info" @keyup.enter="getIPInfo">
+          <span class="cursor-pointer bg-black text-white px-4 rounded-tl-md rounded-bl-md flex items-center" @click="getCurrentIP">
+            <i class="fa-solid fa-circle-notch fa-spin" v-if="currentLocationLoading"></i>
+            <i class="fas fa-map-marker-alt" v-else></i>
+          </span>
+          <input v-model="queryIP" type="text" class="flex-1 py-3 px-3 focus:outline-none" placeholder="Search for any IP address" @keyup.enter="getIPInfo">
 
-          <i class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md fas fa-chevron-right flex items-center" @click="getIPInfo"></i>
+          <span class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center" @click="getIPInfo">
+            <i class="fa-solid fa-circle-notch fa-spin" v-if="loading"></i>
+            <i class="fas fa-chevron-right" v-else></i>
+          </span>
         </div>
       </div>
 
@@ -41,6 +48,8 @@ export default {
     const ipInfo = ref(null)
     const apiKey = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN
     const geoIPKey = process.env.VUE_APP_GEO_IP
+    var currentLocationLoading = false
+    var loading = false
 
     onMounted(() => {
       newMap = leaflet.map('map').setView([27.7172, 85.3240], 13);
@@ -55,6 +64,7 @@ export default {
     })
 
     const getIPInfo = async () => {
+      loading = true
       try {
         const response = await axios.get(`https://geo.ipify.org/api/v1?apiKey=${geoIPKey}&ipAddress=${queryIP.value}`)
         const result = response.data
@@ -68,8 +78,24 @@ export default {
         }
         leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(newMap)
         newMap.setView([ipInfo.value.lat, ipInfo.value.lng], 13)
+        loading = false
       } catch(error) {
         alert(error.message)
+        loading = false
+      }
+    }
+
+    const getCurrentIP = async () => {
+      currentLocationLoading = true
+      try {
+        const response = await axios.get(`https://api.ipify.org/?format=json`)
+        const currentIP = response.data.ip
+        queryIP.value = currentIP
+        getIPInfo()
+        currentLocationLoading = false
+      } catch(error) {
+        alert(error.message)
+        currentLocationLoading = false
       }
     }
 
@@ -78,7 +104,10 @@ export default {
       ipInfo,
       getIPInfo,
       apiKey,
-      geoIPKey
+      geoIPKey,
+      getCurrentIP,
+      currentLocationLoading,
+      loading
     }
   }
 }
